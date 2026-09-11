aaaad2855ec6651deb59b35e9897db9565701a07
squev74 <99035157+squev74@users.noreply.github.com>
2026-09-11 18:41:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/ToDo/', // 👈 Remplace "nom-de-ton-depot" par le nom exact de ton projet GitHub
+})
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
